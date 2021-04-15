3a706e50ab683c3e345af1ed3b69ece967818a15
Lawrence Peters <lawland2020@outlook.com>
2021-04-15 12:40:04 +0100

    Add project files.

---
+++ JokesApp/Models/Jokes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public string jokeQuestion { get; set; }
+        public string jokesAnswer { get; set; }
+
+        public Jokes()
+        {
+
+        }
+    }
+}
